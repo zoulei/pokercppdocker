8a8bd7fdf9c57f984d3e0df33c7f59613f78fe60
zoul15 <346954742@qq.com>
2018-12-07 21:51:51 +0800

    将回车改为了换行

--- build/snap.sh
+++ build/snap.sh
@@ -1,25 +1,5 @@
-#!/usr/bin/env bash
-ORIG=$(pwd)                                                                                                       
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"                                                           
-ROOT="$(dirname $DIR)"
-PROJ="$ROOT/hello-cpp-linwin/"
-STAGE="$ROOT/stage/"
+#!/bin/bash
+sudo docker build --rm -t "zoul15/hellobuild:1" .
+sudo docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it zoul15/hellobuild:1
 
-indent(){
-    sed 's/^/    /'
-}
 
-echo "Taking a 'develop' snapshot first (required for 'build')"
-"$DIR/../develop/snap.sh" | indent
-
-cd "$DIR"
-docker build . -t "hellobuild:latest"
-
-echo "things to try:"
-echo "docker run --rm -it hellobuild /bin/bash"
-echo "docker run --rm hellobuild /usr/local/src/hello/build/testBin/cow_test --log_level=test_suite"
-echo "docker run --rm hellobuild /usr/local/src/hello/build/bin/hello"
-echo "docker run --rm hellobuild /usr/local/src/hello/build/bin/hello --version"
-echo
-
-cd "$ORIG"
--- run/snap.sh
+++ run/snap.sh
@@ -1,9 +1,2 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "Taking snapshot 'run'"
-
-docker build "$DIR" -t "hellorun:latest"
-
-echo "To explore 'run' use:"
-echo "docker run --rm -it hellorun /bin/bash"
-echo
+#!/bin/bash
+sudo docker build --rm -t "zoul15/hellorun:1" .
