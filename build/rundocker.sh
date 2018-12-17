a5b60f041beabc9e3724dcdb1a51e90078aebea2
leizou <346954742@qq.com>
2018-12-18 01:08:46 +0800

    增加一些sh脚本

---
+++ build/rundocker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it zoul15/hellobuild:1
+
+
--- pycode/rundocker.sh
+++ pycode/rundocker.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-sudo docker build --rm -t "zoul15/hellopy:1" .
 sudo docker run -it zoul15/hellopy:1 /bin/bash
