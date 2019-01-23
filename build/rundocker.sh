c84cfc909e0a2744714bd3850bea9e9ff4292d3e
zoul15 <346954742@qq.com>
2019-01-23 20:34:24 +0800

    同步代码

--- build/rundocker.sh
+++ build/rundocker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it zoul15/hellobuild:1
+sudo docker run -v /home/zoul15/pccppdocker/build/testdir:/home/zoul15/testdir --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it zoul15/hellobuild:1
 
 
