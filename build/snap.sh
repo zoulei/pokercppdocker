81cf04ebb8acb6d17ca204799109785b1db919f1
zoul15 <346954742@qq.com>
2019-01-25 16:09:12 +0800

    同步代码

--- build/snap.sh
+++ build/snap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo docker build --rm -t "zoul15/hellobuild:1" .
-sudo docker run -v /home/zoul15/pccppdocker/build/testdir:/home/zoul15/testdir --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it zoul15/hellobuild:1
+sudo docker run -v /home/zoul15/pccppdocker/build/testdir:/home/zoul15/testdir -v /home/zoul15/pcshareddir:/home/zoul15/pcshareddir --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it zoul15/hellobuild:1
 
 
