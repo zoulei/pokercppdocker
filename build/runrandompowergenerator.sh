13dc5caccd1679b8d6f1d3481c5c2c93b15b5bbe
zoul15 <346954742@qq.com>
2019-02-14 00:44:52 +0800

    增加了将主机的名字设置为docker中的环境变量

--- build/runrandompowergenerator.sh
+++ build/runrandompowergenerator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it zoul15/hellobuild:generaterandomhandpower
+sudo docker run -e HOST_HOSTNAME=`hostname` --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it zoul15/hellobuild:generaterandomhandpower
