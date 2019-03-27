221f88815a02b540998281066e7a9400c1a5e0fc
zoul15 <346954742@qq.com>
2019-03-27 10:39:40 +0800

    同步代码

--- build/rundocker.sh
+++ build/rundocker.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo docker run -e HOST_HOSTNAME=`hostname` -v /home/zoul15/pccppdocker/build/testdir:/home/zoul15/testdir -v /home/zoul15/pcshareddir:/home/zoul15/pcshareddir --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -it pustudy/hellobuild:1
+sudo docker run -e HOST_HOSTNAME=`hostname` \
+-v /home/zoul15/pccppdocker/build/testdir:/home/zoul15/testdir \
+-v /home/zoul15/pcshareddir:/home/zoul15/pcshareddir \
+--cap-add=SYS_PTRACE \
+--security-opt seccomp=unconfined \
+-it pustudy/hellobuild:1
