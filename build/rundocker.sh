08af4f4c79d32ec8a0b4958bd0a0e572a54067db
zoulei <346954742@qq.com>
2019-06-30 16:37:32 +0800

    同步代码

--- build/rundocker.sh
+++ build/rundocker.sh
@@ -3,6 +3,7 @@ sudo docker run -e HOST_HOSTNAME=`hostname` \
 -v /home/zoul15/pccppdocker/build/pokerweb:/home/zoul15/pokerweb \
 -v /home/zoul15/pccppdocker/build/testdir:/home/zoul15/testdir \
 -v /home/zoul15/pcshareddir:/home/zoul15/pcshareddir \
+-v /home/zoul15/example:/home/zoul15/tensorflow-master/tensorflow/cc/example \
 --cap-add=SYS_PTRACE \
 --security-opt seccomp=unconfined \
 -it pustudy/hellobuild:1
